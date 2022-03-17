c012d3e6196cce6d9cc3f21b1a946546b4afd35f
p000059 <marcelo.acad@gmail.com>
2022-03-17 02:17:07 -0300

    20220317021600AM

---
+++ nodejs/MObject.js
@@ -0,0 +1,8 @@
+var objCar = {
+    brand: "GM",
+    model: "Camaro",
+    year: 2012,
+    color: 'Yellow'
+}
+
+module.exports = objCar;
--- nodejs/NNode.js
+++ nodejs/NNode.js
@@ -1,6 +1,17 @@
 var http = require('http');
 
+var dt = require('./MObject');
+
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
+  res.write("Marca: " + dt.brand);
+  res.write("<br/>Modelo: " + dt.model);
+  res.write("<br/>Ano: " + dt.year);
+  res.write("<br/>Cor: " + dt.color);
+  res.end();
 }).listen(8080);
+
+exports.myDateTime = function () {
+  return new Date().toLocaleTimeString();
+};
+
